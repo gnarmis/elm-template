0d2bad27475ea4a00cdf2166950dbabe7e5c0e09
Renee Marie <renee.blunt@gmail.com>
2018-10-16 09:17:42 -0700

    progress

--- src/Main.elm
+++ src/Main.elm
@@ -1,9 +1,21 @@
 module Main exposing (Model)
 
+-- start the following are needed for fetching and decoding the data
+-- https://github.com/lukewestby/elm-http-builder
+-- import Http
+-- import HttpBuilder exposing (..)
+-- import Json.Decode as Decode
+-- import Json.Encode as Encode
+-- end
+
 import Browser
 import Browser.Navigation as Nav
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import Http
+import HttpBuilder exposing (..)
+import Json.Decode as Decode
+import Json.Encode as Encode
 import Url
 
 
@@ -14,6 +26,7 @@ type alias Model =
 type Msg
     = LinkClicked Browser.UrlRequest
     | ChangedUrl Url.Url
+    | Funsies Result
 
 
 init : () -> Url.Url -> Nav.Key -> ( Model, Cmd Msg )
@@ -38,6 +51,36 @@ subscriptions _ =
     Sub.none
 
 
+
+-- Missions --
+
+
+type alias Mission =
+    { id : Int
+    , domainId : Int
+    , gradeLevelId : Int
+    }
+
+-- decoders have to be in the same order as the model properties
+-- https://package.elm-lang.org/packages/elm-lang/core/5.1.1/Json-Decode#map2
+
+missionDecoder : Decode.Decoder Mission
+missionDecoder =
+    Decode.map3 Mission
+        (Decode.field "id" Decode.int)
+        (Decode.field "domain_id" Decode.int)
+        (Decode.field "grade_level_id" Decode.int)
+
+-- handleRequestComplete : Result Http.Error (List String) -> Msg
+-- handleRequestComplete result =
+--     Funsies result
+    -- something here
+
+getMissions : String -> Cmd Msg
+getMissions =
+    HttpBuilder.get "http://localhost:3000/missions"
+        |> HttpBuilder.withExpectJson missionDecoder
+        |> Http.send Funsies
 
 -- MAIN
 
